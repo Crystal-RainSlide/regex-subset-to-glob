6afd7b2461d0694d3852c7e9999f6fa51b920d34
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-07-08 18:06:53 +0800

    fixup! Initial push

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-import regexSubsetToGlob   from "regex-subset-to-glob.js";
-import regexSubsetToGlobOR from "regex-subset-to-glob-OR.js";
+import regexSubsetToGlob   from "./regex-subset-to-glob.js";
+import regexSubsetToGlobOR from "./regex-subset-to-glob-OR.js";
 
 // for quickjs
-if ( (log in console) && !(error in console) ) console.error = console.log;
+if ( ("log" in console) && !("error" in console) ) console.error = console.log;
 
 const inputs = ["a[bc][d]e[fg-w]x.y.*z", "[a|b]c|d[e]||f", "[", "]", "[]"];
 
